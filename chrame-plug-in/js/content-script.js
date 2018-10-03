a189c1adcc197fef43bf455b906de6d3d31a6bbe
DESKTOP-I214FNK\caiwe <caiwendi88@gmail.com>
2018-10-03 13:03:57 +0800

    插件

---
+++ chrame-plug-in/js/content-script.js
@@ -0,0 +1,116 @@
+﻿// 'use strict';
+
+// 注意，必须设置了run_at=document_start 此段代码才会生效
+console.log('这是content script!');
+console.log(window.location);
+
+// let username = document.cookie.split(";")[0].split("=")[1];
+//JS操作cookies方法!
+//写cookies
+function setCookie(name, value) {
+    var Days = 30;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+}
+
+function getCookie(name) {
+    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+    if (arr = document.cookie.match(reg)) {
+        return unescape(arr[2]);
+    } else {
+        return null;
+    }
+}
+
+function chrome_isLocation() {
+    let chromeLocationHostArray = ['www.22gvb.com', 'www.00rfd.com'];
+    let chrome_isActive = false;
+    for (let i = 0; i < chromeLocationHostArray.length; i++) {
+        if (window.location.host == chromeLocationHostArray[i]) {
+            chrome_isActive = true;
+            console.log('chrome——要监听的网站');
+        }
+    }
+    return chrome_isActive
+}
+
+let chrome_isLocationS = chrome_isLocation();
+
+window.onload = function () {
+    if (chrome_isLocationS) {
+        // chrome_login()
+    }
+    console.log(111);
+    console.log(chrome.extension);
+}
+
+// DOMContentLoaded 页面加载完毕
+window.addEventListener('DOMContentLoaded', function () {
+    // 注入自定义JS
+    if (chrome_isLocationS) {
+        injectCustomJs();
+        injectCustomJs("js/jquery-3.3.1.min.js");
+        initCustomPanel();
+        setTimeout(_ => {
+            console.log('chrome——', window.document.cookie);
+            console.log('chrome——', getCookie('__cfduid'));
+            console.log('chrome——', getCookie('_ga'));
+            console.log('chrome——', getCookie('_gat'));
+            console.log('chrome——', getCookie('_gid'));
+            console.log('chrome——', getCookie('sessionId'));
+        }, 1200)
+    }
+    console.log('DOMContentLoaded');
+});
+
+function initCustomPanel() {
+    var panel = document.createElement('div');
+    panel.className = 'inject-panel';
+    panel.innerHTML = `
+		<h2>inject-panel：</h2>
+		<div class="btn-area" style="margin-bottom: 60px;">
+            <a href="javascript:queryDeskDetail()" style="color: #545454">queryDeskDetail</a>
+            <a href="javascript:startListen()" style="color: #545454">startListen</a>
+        </div>
+        <button id="chromeLoginClick" type="button" onclick="chrome_login()" style="color: black">获取验证码并解析赋值</button>	
+        <button type="button" onclick="qagetIndex()" style="color: black">同意的确认</button>	
+        <button type="button" onclick="qasetCookie()" style="color: black">设置COOKIE</button>
+        <button type="button" onclick="selectedYuan()" style="color: black">选择下注金额</button>	
+`;
+    document.body.appendChild(panel);
+}
+
+// 向页面注入CSS
+function loadStyle(url) {
+    var link = document.createElement('link');
+    link.setAttribute('type', 'text/css');
+    link.setAttribute('rel', 'stylesheet');
+    link.href = chrome.extension.getURL(url);
+    link.onload = function () {
+        this.parentNode.removeChild(this);
+    };
+    document.body.appendChild(link);
+}
+
+// 向页面注入JS
+function injectCustomJs(jsPath) {
+    jsPath = jsPath || 'js/inject.js';
+    var temp = document.createElement('script');
+    temp.setAttribute('type', 'text/javascript');
+    temp.src = chrome.extension.getURL(jsPath);
+    temp.onload = function () {
+        this.parentNode.removeChild(this);
+    };
+    document.body.appendChild(temp);
+}
+
+function selectedYuan() {
+    //
+    // $($("._2oHIg ._2Eb76:not(._3BVsE)")[0]).children().click(); // 选择筹码
+    // //
+    // $($('._3Y07G').children()[0]).find('._34Nqi.ZUikl._67CnM').click(); // 选择牌
+    // //
+    // $($('._3Y07G').children()[0]).find('._34Nqi.ZUikl._67CnM').find('._1a9j-._1m_7V').click(); // 确认下注
+}
+
---
+++ chrame-plug-in/js/inject.js
@@ -0,0 +1,307 @@
+var datamap = {};
+datamap["_3m2-b"] = "A1";
+datamap["_1mjUZ"] = "A2";
+datamap["_2k309"] = "A3";
+datamap["_3jhEf"] = "A4";
+datamap["_15JYj"] = "A5";
+datamap["_1YmG6"] = "A6";
+datamap["_138Ip"] = "A7";
+datamap["_19CK0"] = "A8";
+datamap["_1FAb8"] = "A9";
+datamap["_3bR2d"] = "A10";
+datamap["_20aw3"] = "A11";
+datamap["_2psWU"] = "A12";
+datamap["O4fLT"] = "A13";
+datamap["_2jmcJ"] = "B1";
+datamap["_2i1TV"] = "B2";
+datamap["_3E9yR"] = "B3";
+datamap["cM8ah"] = "B4";
+datamap["mjgRe"] = "B5";
+datamap["_39Q6j"] = "B6";
+datamap["_29X7a"] = "B7";
+datamap["_37S_a"] = "B8";
+datamap["_3_O7D"] = "B9";
+datamap["_3Z457"] = "B10";
+datamap["_3QJhk"] = "B11";
+datamap["_13sJi"] = "B12";
+datamap["_2LuRH"] = "B13";
+datamap["-gItF"] = "C1";
+datamap["_1wqeR"] = "C2";
+datamap["_2L_yA"] = "C3";
+datamap["_1jMhL"] = "C4";
+datamap["wZugz"] = "C5";
+datamap["NvSPk"] = "C6";
+datamap["_1Nevk"] = "C7";
+datamap["_2xxWN"] = "C8";
+datamap["_17-7k"] = "C9";
+datamap["_3InVY"] = "C10";
+datamap["_2Q6Dz"] = "C11";
+datamap["_1EdIj"] = "C12";
+datamap["_2rErY"] = "C13";
+datamap["_1EJ1e"] = "D1";
+datamap["_2qr2W"] = "D2";
+datamap["_13s0z"] = "D3";
+datamap["_3FpIt"] = "D4";
+datamap["_1k4p5"] = "D5";
+datamap["_3axgC"] = "D6";
+datamap["_3mtFZ"] = "D7";
+datamap["_3GZUt"] = "D8";
+datamap["_3CcEJ"] = "D9";
+datamap["_2mlUQ"] = "D10";
+datamap["_2zy_e"] = "D11";
+datamap["_2UskY"] = "D12";
+datamap["_3LInw"] = "D13";
+
+var winmap = {};
+winmap["_322Py"] = "闲对";
+winmap["_7vTww"] = "闲";
+winmap["_3xcd"] = "和";
+winmap["ZUikl"] = "庄";
+winmap["EUsyn"] = "庄对";
+winmap["_2hqy3"] = "龙";
+winmap["_3g4fx"] = "和";
+winmap["_1xv2M"] = "虎";
+
+
+
+
+window.chrome_login_User_Obj = {
+    userName: 'nin23801',
+    password: 'aabb123123',
+}
+//
+window.onload = function () {
+    console.log('页面加载完毕111111');
+    var chrome_yzImg = $('._3IDPG ._1CgIs._1I2Om img');
+    setTimeout(() => {
+        chrome_login()
+    }, 3000)
+}
+
+function queryDeskDetail() {
+    console.log("234324start queryDeskDetail");
+    var rtndata = [];
+    var deskes = $("[class='_6VpXo']");
+    for (var i = 0; i < deskes.length; i++) {
+        var ps = $(deskes[i]).find(".e93on > ._2Qhx2 > ._1DTtZ._1Z5TM > ._3_Q04 > ._1I_B5");
+        var left = $(ps).find("._5yJ9E > ._3QN0V > span");
+        var right = $(ps).find(".Wa9oZ > ._3QN0V > span");
+
+        var arrleft = [];
+        $(left[0]).find("div").each(function (i, d) {
+            var clazz = $(d).attr("class");
+            if (clazz) {
+                var p = clazz.split(" ")[1];
+                var data = datamap[p];
+                arrleft.push(data);
+                //console.log(data);
+            }
+        });
+        $(left[1]).find("div").each(function (i, d) {
+            var clazz = $(d).attr("class");
+            if (clazz) {
+                var p = clazz.split(" ")[1];
+                var data = datamap[p];
+                arrleft.push(data);
+                // console.log(data);
+            }
+        });
+        var arrright = [];
+        $(right[0]).find("div").each(function (i, d) {
+            var clazz = $(d).attr("class");
+            if (clazz) {
+                var p = clazz.split(" ")[1];
+                var data = datamap[p];
+                arrright.push(data);
+                // console.log(data);
+            }
+        });
+        $(right[1]).find("div").each(function (i, d) {
+            var clazz = $(d).attr("class");
+            if (clazz) {
+                var p = clazz.split(" ")[1];
+                var data = datamap[p];
+                arrright.push(data);
+                // console.log(data);
+            }
+        });
+
+        var desk = {};
+        desk.left = arrleft;
+        desk.right = arrright;
+        rtndata.push(desk);
+    }
+
+    console.log(JSON.stringify(rtndata));
+    return rtndata;
+}
+
+function formatterDateTime() {
+    var date = new Date()
+    var month = date.getMonth() + 1
+    var datetime = date.getFullYear()
+        + ""// "年"
+        + (month >= 10 ? month : "0" + month)
+        + ""// "月"
+        + (date.getDate() < 10 ? "0" + date.getDate() : date
+            .getDate())
+        + ""
+        + (date.getHours() < 10 ? "0" + date.getHours() : date
+            .getHours())
+        + ""
+        + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date
+            .getMinutes())
+        + ""
+        + (date.getSeconds() < 10 ? "0" + date.getSeconds() : date
+            .getSeconds());
+    return datetime;
+}
+
+function chrome_login() {
+    // let base64Img = document.querySelector('._3IDPG ._1CgIs._1I2Om img').src; // 验证码
+    // $.ajax({
+    //     type: 'post',
+    //     url: 'http://route.showapi.com/184-5',
+    //     dataType: 'json',
+    //     data: {
+    //         "showapi_timestamp": formatterDateTime(),
+    //         "showapi_appid": '75499', //这里需要改成自己的appid
+    //         "showapi_sign": '3be7fb2bd58b4dbca0c8c6a028fbcff0',  //这里需要改成自己的应用的密钥secret
+    //         "img_base64": base64Img,
+    //         "typeId": "34",
+    //         "convert_to_jpg": "0",
+    //         "needMorePrecise": "0"
+    //     },
+    //     success: function (result) {
+    //         let resCode = result.showapi_res_body.Result;
+    //         window.kv = new KeyboardEvent('input', {bubbles: true});
+    //         window.el1 = $("._3IDPG input._3pu_i")[0];
+    //         el1.value = window.chrome_login_User_Obj.userName;
+    //         el1._valueTracker.setValue(el1);
+    //         el1.dispatchEvent(kv);
+    //         window.el2 = $("._3IDPG input._39jpe")[0];
+    //         el2.value = window.chrome_login_User_Obj.password;
+    //         el2._valueTracker.setValue(el2);
+    //         el2.dispatchEvent(kv);
+    //         window.el3 = $("._3IDPG input._2LWcS")[0];
+    //         el3.value = resCode;
+    //         el3._valueTracker.setValue(el3);
+    //         el3.dispatchEvent(kv);
+    //         console.log('123', el1, el2, el3)
+    //         $("._3IDPG button").click();
+    //         setTimeout(() => {
+    //             $('._3IR2e ._3MSiK').click();
+    //         }, 3000)
+    //     },
+    //     error: function (XmlHttpRequest, textStatus, errorThrown) {
+    //         alert("操作失败!");
+    //     }
+    // })
+}
+
+var callback = function(mutationsList) { 
+    for(var mutation of mutationsList) { 
+        //console.log(mutation); 
+        console.log("=====================");
+        if (mutation.type == 'attributes') { 
+            var classData = mutation.target.className;
+            console.log("classData:" + classData);
+            var t = $(mutation.target).parents("._6VpXo");
+
+            var rtndata = analysisData(t);
+            console.log(JSON.stringify(rtndata));
+        }
+
+        observer.observe(mutation.target, config);
+    } 
+}; 
+// var config = { attributes: true}; 
+var config = { attributes: true };
+var observer = new MutationObserver(callback); 
+
+function startListen() { 
+    console.log("start listen");
+
+    var targets = $(".qMFBr div[role!='button']:first-child");
+    console.log(targets.length);
+
+    for(var i=0;i<targets.length;i++) {
+        console.log(targets[i]);
+        observer.observe(targets[i], config); 
+    }
+}
+
+
+function analysisData(deskNode) {
+
+    var ps = $(deskNode).find(".e93on > ._2Qhx2 > ._1DTtZ._1Z5TM > ._3_Q04 > ._1I_B5");
+    var left = $(ps).find("._5yJ9E > ._3QN0V > span");
+    var right = $(ps).find(".Wa9oZ > ._3QN0V > span");
+
+    var arrleft = [];
+    $(left[0]).find("div").each(function (i, d) {
+        var clazz = $(d).attr("class");
+        if (clazz) {
+            var p = clazz.split(" ")[1];
+            var data = datamap[p];
+            arrleft.push(data);
+            //console.log(data);
+        }
+    });
+    $(left[1]).find("div").each(function (i, d) {
+        var clazz = $(d).attr("class");
+        if (clazz) {
+            var p = clazz.split(" ")[1];
+            var data = datamap[p];
+            arrleft.push(data);
+            // console.log(data);
+        }
+    });
+    var arrright = [];
+    $(right[0]).find("div").each(function (i, d) {
+        var clazz = $(d).attr("class");
+        if (clazz) {
+            var p = clazz.split(" ")[1];
+            var data = datamap[p];
+            arrright.push(data);
+            // console.log(data);
+        }
+    });
+    $(right[1]).find("div").each(function (i, d) {
+        var clazz = $(d).attr("class");
+        if (clazz) {
+            var p = clazz.split(" ")[1];
+            var data = datamap[p];
+            arrright.push(data);
+            // console.log(data);
+        }
+    });
+
+    var desk = {};
+    desk.left = arrleft;
+    desk.right = arrright;
+    
+    var deskNameData = $(deskNode).find("._3fFvD span");
+    if(deskNameData.length == 2){
+        desk.name1 = $(deskNameData[0]).text();
+        desk.name2 = $(deskNameData[1]).text();
+    } else {
+        desk.name1 = $(deskNameData[0]).text();
+        desk.name2 = "";
+    }
+
+    var countData = $(deskNode).find("._1tFN6").text().split("-");
+    desk.count1 = countData[0];
+    desk.count2 = countData[1];
+
+    var winNodes = $(deskNode).find("div._3AxTi > div.i6ChJ > div._3jUwm");
+    var winDatas = [];
+    for(var i=0;i<winNodes.length;i++) {
+        var index = $(winNodes[i]).attr("class").split(" ")[1];
+        var data = winmap[index];
+        winDatas.push(data);
+    }
+    desk.windatas = winDatas;
+
+    return desk;
+}
