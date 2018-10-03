e7d02037a131dcdeaca5fc520e26a68932b0fde2
lbj <jiawei.xie@infaith.com.cn>
2018-10-03 21:25:59 +0800

    修改初始化主流程

--- bdt/src/main/java/cn/com/infaith/module/service/BJLDataService.java
+++ bdt/src/main/java/cn/com/infaith/module/service/BJLDataService.java
@@ -52,19 +52,60 @@ public class BJLDataService {
 
 
     /**
-     * 根据桌号判断状态
-     * @param tableNo
+     * 判断状态
+     * @param tableNo   当前桌号
+     * @param battleNo  当前局号
+     * @param fitNo     当前副号
+     * @param status    当前状态
+     * @param tzSystem  投注系统
      */
-    public void JudgeState(int tableNo) {
+    public void JudgeState(int tableNo, int battleNo, int fitNo, int status, int tzSystem) {
         //获取当前桌的最新状态
         StatusData statusData = tableDataService.getStatusByTableNo(tableNo);
         if (statusData == null) {
             initTableData();
-            JudgeState(tableNo);
+            JudgeState(tableNo, battleNo, fitNo, status, tzSystem);
         } else {
             //判断状态
-            statusData.getStatus();
+            int state = statusData.getStatus();
+            //状态不变。进入步骤15。
+            if (state == status) {
+                return;
+            }
+            //状态改变，当前状态为“可投注”（局号、副号不会变）。
+            if (state != status && state == TableStatusEnum.TZ.getIndex()) {
+                tzStatus(tableNo, battleNo, fitNo, status, tzSystem);
+            }
+            //状态改变，当前状态为“新局准备”（局号改变、副号变为1）。
+            if (state != status && state == TableStatusEnum.NEW.getIndex()) {
+                newReady(tableNo, battleNo, fitNo, status, tzSystem);
+            }
+            //状态改变，当前状态为“开牌”（局号、副号不会变）。
+            if (state != status && state == TableStatusEnum.KP.getIndex()) {
+                openCard(tableNo, battleNo, fitNo, status, tzSystem);
+            }
         }
+    }
+
+    /**
+     * 可投注状态
+     */
+    private void tzStatus(int tableNo, int battleNo, int fitNo, int status, int tzSystem) {
+
+    }
+
+    /**
+     * 新局准备状态
+     */
+    private void newReady(int tableNo, int battleNo, int fitNo, int status, int tzSystem) {
+
+    }
+
+    /**
+     * 开牌状态
+     */
+    private void openCard(int tableNo, int battleNo, int fitNo, int status, int tzSystem) {
+
     }
 
 
